653cf51eb39913dcabbb5aa8b2233292810d38d4
Stanislav Adams <adams.stanislav@gmail.com>
2023-08-11 17:12:26 +0300

    Initial Commit

---
+++ sagetoj/sagetojApp.swift
@@ -0,0 +1,17 @@
+//
+//  sagetojApp.swift
+//  sagetoj
+//
+//  Created by Stanislav Adams on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct sagetojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
