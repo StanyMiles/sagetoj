1cd38557c30fa3b2ba9365fbc537f5cf760152ae
Stanislav Adams <adams.stanislav@gmail.com>
2023-08-16 15:13:39 +0300

    Handle keyboard appearance

--- sagetoj/Views/GameView.swift
+++ sagetoj/Views/GameView.swift
@@ -3,51 +3,54 @@ import SwiftUI
 struct GameView: View {
   @EnvironmentObject private var coordinator: Coordinator
   @StateObject var viewModel: ViewModel
+  @FocusState private var isFocused: Bool
   
   var body: some View {
-    VStack(spacing: 60) {
-      HStack(spacing: 30) {
-        ForEach(viewModel.scores) { playerScore in
-          VStack {
-            Text(playerScore.player.name)
-              .font(.headline)
-            Text(String(playerScore.score))
-              .font(.title.bold())
+    ScrollView {
+      VStack(spacing: 20) {
+        HStack(spacing: 30) {
+          ForEach(viewModel.scores) { playerScore in
+            VStack {
+              Text(playerScore.player.name)
+                .font(.headline)
+              Text(String(playerScore.score))
+                .font(.title.bold())
+            }
+            .foregroundColor(viewModel.isActive(playerScore.player) ? .green : .black)
           }
-          .foregroundColor(viewModel.isActive(playerScore.player) ? .green : .black)
         }
+        
+        HStack(spacing: 40) {
+          throwView("Throw 1", isActive: viewModel.throwCount == 1)
+          throwView("Throw 2", isActive: viewModel.throwCount == 2)
+          throwView("Throw 3", isActive: viewModel.throwCount == 3)
+        }
+        
+        TextField("Input", text: $viewModel.throwResult)
+          .multilineTextAlignment(.center)
+          .keyboardType(.numberPad)
+          .textFieldStyle(.roundedBorder)
+          .font(.largeTitle)
+          .focused($isFocused)
+          .padding(.horizontal, 30)
+          .onSubmit(onAddScore)
+        
+        VStack {
+          Toggle("Double", isOn: $viewModel.isDouble)
+          Toggle("Triple", isOn: $viewModel.isTriple)
+        }
+        .padding(.horizontal)
+        
+        Button("Add score", action: onAddScore)
+          .font(.largeTitle)
+          .buttonStyle(.borderedProminent)
+        
+        HStack(spacing: 40) {
+          Button("Add Single BULL", action: viewModel.onSingleBull)
+          Button("Add Double BULL", action: viewModel.onDoubleBull)
+        }
+        .buttonStyle(.bordered)
       }
-      
-      HStack(spacing: 40) {
-        throwView("Throw 1", isActive: viewModel.throwCount == 1)
-        throwView("Throw 2", isActive: viewModel.throwCount == 2)
-        throwView("Throw 3", isActive: viewModel.throwCount == 3)
-      }
-      
-      TextField("Input", text: $viewModel.throwResult)
-        .multilineTextAlignment(.center)
-        .keyboardType(.numberPad)
-        .textFieldStyle(.roundedBorder)
-        .font(.largeTitle)
-        .padding(.horizontal, 30)
-      
-      HStack(spacing: 40) {
-        Button("Add Single BULL", action: viewModel.onSingleBull)
-        Button("Add Double BULL", action: viewModel.onDoubleBull)
-      }
-      .buttonStyle(.bordered)
-      
-      VStack {
-        Toggle("Double", isOn: $viewModel.isDouble)
-        Toggle("Triple", isOn: $viewModel.isTriple)
-      }
-      .padding(.horizontal)
-      
-      Button("Add score", action: viewModel.onAddScore)
-        .font(.largeTitle)
-        .buttonStyle(.borderedProminent)
-      
-      Spacer()
     }
     .navigationTitle("Throw your darts")
     .navigationBarTitleDisplayMode(.inline)
@@ -75,12 +78,17 @@ struct GameView: View {
     }
   }
   
-  func throwView(_ text: String, isActive: Bool) -> some View {
+  private func throwView(_ text: String, isActive: Bool) -> some View {
     VStack {
       Text(text)
         .font(isActive ? .title.bold() : .title2)
       
     }
+  }
+  
+  private func onAddScore() {
+    isFocused = false
+    viewModel.onAddScore()
   }
 }
 
--- sagetoj/Views/StarterView.swift
+++ sagetoj/Views/StarterView.swift
@@ -4,14 +4,15 @@ struct StarterView: View {
   @EnvironmentObject private var coordinator: Coordinator
   
   var body: some View {
-    Button("Start game") {
-      coordinator.start()
-    }
+    Button("Start game", action: coordinator.start)
+      .buttonStyle(.borderedProminent)
+      .font(.title)
   }
 }
 
 struct StarterView_Previews: PreviewProvider {
   static var previews: some View {
     StarterView()
+      .environmentObject(Coordinator())
   }
 }
